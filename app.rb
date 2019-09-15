486a7c7c98df41cd5aa1bdd3b32cb8633c227920
Raza <rtshareef@gmail.com>
2019-09-15 01:56:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 class App < Sinatra::Base
 
-  # Define your GET '/' route below and respond with "Hello, World!"
+  # Defineclass Application < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/hey' do
+    "Heyyyyy"
+  end
+
+
+  # ! http://localhost:9393/hey - will give you 'Heyyyyy'
 
 
 end
